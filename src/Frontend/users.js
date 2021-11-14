42c2ce9e0ebdee63fd86024e542e775ac7d51e6c
ElayGelbart <elaygelbart@gmail.com>
2021-11-14 19:02:16 +0200

    Basic Front Login

---
+++ src/Frontend/users.js
@@ -0,0 +1,27 @@
+'use strict'
+// const Host = `https://egshorturl.herokuapp.com`;
+const Host = `http://localhost:8080`;
+
+const signupToWebsite = () => {
+  const usernameValue = document.getElementById("signupUsernameInput").value;
+  const passwordValue = document.getElementById("signupPasswordInput").value;
+  const rePasswordValue = document.getElementById("signupRePasswordInput").value;
+  if (passwordValue != rePasswordValue) {
+    createErrorInputUI(document.getElementById("signupRePasswordInput"), "Passwords don't match");
+    return
+  }
+  if (!validator.isLength(usernameValue, { min: 3, max: 15 }) || !validator.isAlphanumeric(newURLvalue)) {
+    createErrorInputUI(document.getElementById("signupUsernameInput"), "Username invalid");
+    return
+  }
+  if (!validator.isStrongPassword(passwordValue, { minLength: 8, maxLength: 30, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })) {
+    createErrorInputUI(document.getElementById("signupPasswordInput"), "Password not Strong enough");
+    return
+  }
+  try {
+    const response = await axios.post(`${Host}/signup/newuser`, { username: usernameValue, password: passwordValue });
+    clearBadInput();
+  } catch (err) {
+    createErrorInputUI(document.getElementById("signupUsernameInput"), "Username Taken");
+  }
+}
