ff2ecff12126828027c0e99d3d173bcfabbe5cc3
ElayGelbart <elaygelbart@gmail.com>
2021-11-17 10:38:16 +0200

    EGShortURL V1.6.1 - Style Good

--- src/Frontend/getUser.js
+++ src/Frontend/getUser.js
@@ -3,7 +3,7 @@ const CookieArr = Cookie.split(";");
 for (let value of CookieArr) {
   if (/username/.test(value)) {
     const CookieUserName = value.split("=")[1];
-    document.getElementById("userGreeting").innerText = `hello ${CookieUserName}`;
+    document.getElementById("userGreeting").innerText = `${CookieUserName}`;
     break;
   }
 
